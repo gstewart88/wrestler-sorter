ccb3bdf79d37eba01d9e05b1d9641af18739e5f9
Graeme Stewart <graemes88@hotmail.com>
2025-09-11 16:53:22 +0100

    link to web rather than save images

--- frontend/src/components/CompanyFilter.js
+++ frontend/src/components/CompanyFilter.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './CompanyFilter.css';
 
 const companyLogos = {
-  AEW:      '/images/companies/AEW.png',
-  WWE:      '/images/companies/WWE.png',
-  NJPW:     '/images/companies/NJPW.png',
-  Stardom:  '/images/companies/Stardom.png',
-  Marigold: '/images/companies/Marigold.png',
-  TJPW:     '/images/companies/TJPW.png',
+  AEW:      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/All_Elite_Wrestling_logo_2023.png/1600px-All_Elite_Wrestling_logo_2023.png',
+  WWE:      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/WWE_Logo.svg/438px-WWE_Logo.svg.png?20180410221710',
+  NJPW:     'https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/New_Japan_Pro_Wrestling_Logo_2.svg/1280px-New_Japan_Pro_Wrestling_Logo_2.svg.png',
+  Stardom:  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Stardom_2021_logo.svg/260px-Stardom_2021_logo.svg.png',
+  Marigold: 'https://dsf-marigold.com/wp-content/themes/marigold/assets/images/mg_logo2.png',
+  TJPW:     'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/TJPW_logo.svg/480px-TJPW_logo.svg.png?20230218193820',
 };
 
 function CompanyFilter({ companies, selected, onToggle }) {
