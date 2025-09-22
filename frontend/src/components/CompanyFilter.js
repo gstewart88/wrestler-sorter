8b7616997a8ef00b8d992cc503787c9850af9788
Graeme Stewart <graemes88@hotmail.com>
2025-09-22 13:06:43 +0100

    attempting fix again

--- frontend/src/components/CompanyFilter.js
+++ frontend/src/components/CompanyFilter.js
@@ -1,8 +1,16 @@
-// src/components/CompanyFilter.js
-
 import React from 'react';
 import { Form } from 'react-bootstrap';
 import './CompanyFilter.css';
+
+ const companyLogos = {
+  AEW:      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/All_Elite_Wrestling_logo_2023.png/1600px-All_Elite_Wrestling_logo_2023.png',
+  Raw:      'https://upload.wikimedia.org/wikipedia/commons/a/a5/WWE_RAW_Logo.png',
+  Smackdown:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/WWE_SmackDown_%282024%29_Logo.svg/2560px-WWE_SmackDown_%282024%29_Logo.svg.png',
+  NJPW:     'https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/New_Japan_Pro_Wrestling_Logo_2.svg/1280px-New_Japan_Pro_Wrestling_Logo_2.svg.png',
+  Stardom:  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Stardom_2021_logo.svg/260px-Stardom_2021_logo.svg.png',
+  Marigold: 'https://dsf-marigold.com/wp-content/themes/marigold/assets/images/mg_logo2.png',
+  TJPW:     'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/TJPW_logo.svg/480px-TJPW_logo.svg.png?20230218193820',
+};
 
 /**
  * A single checkbox that can also render in an “indeterminate” (dash) state.
