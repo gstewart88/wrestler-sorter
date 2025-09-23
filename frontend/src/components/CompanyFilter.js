f488fd85ebf774f8bcf0207930ee5152088f05f7
Graeme Stewart <graemes88@hotmail.com>
2025-09-23 22:11:10 +0100

    adding TNA and NXT

--- frontend/src/components/CompanyFilter.js
+++ frontend/src/components/CompanyFilter.js
@@ -6,6 +6,8 @@ import './CompanyFilter.css';
   AEW:      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/All_Elite_Wrestling_logo_2023.png/1600px-All_Elite_Wrestling_logo_2023.png',
   Raw:      'https://upload.wikimedia.org/wikipedia/commons/a/a5/WWE_RAW_Logo.png',
   Smackdown:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/WWE_SmackDown_%282024%29_Logo.svg/2560px-WWE_SmackDown_%282024%29_Logo.svg.png',
+  NXT:      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/WWE_NXT_2024_Logo.svg/598px-WWE_NXT_2024_Logo.svg.png',
+  TNA:      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/TNA_Wrestling_%282024%29_Logo.svg/1024px-TNA_Wrestling_%282024%29_Logo.svg.png',
   NJPW:     'https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/New_Japan_Pro_Wrestling_Logo_2.svg/1280px-New_Japan_Pro_Wrestling_Logo_2.svg.png',
   Stardom:  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Stardom_2021_logo.svg/260px-Stardom_2021_logo.svg.png',
   Marigold: 'https://dsf-marigold.com/wp-content/themes/marigold/assets/images/mg_logo2.png',
